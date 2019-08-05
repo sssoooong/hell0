d5fb886c567c508bdbc804b393de921b368a2a7f
Minjingg <53463420+Minjingg@users.noreply.github.com>
2019-08-05 16:14:19 +0900

    Fove
    포브

---
+++ Assets/FOVE3DCursor.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class FOVE3DCursor : FOVEBehavior
+{
+    public enum LeftOrRight
+    {
+        Left,
+        Right
+    }
+
+    [SerializeField]
+    public LeftOrRight whichEye;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Latepdate ensures that the object doesn't lag behind the user's head motion
+    void Update()
+    {
+        var rays = FoveInterface.GetGazeRays();
+        var ray = whichEye == LeftOrRight.Left ? rays.left : rays.right;
+
+        RaycastHit hit;
+        Physics.Raycast(ray, out hit, Mathf.Infinity);
+        if (hit.point != Vector3.zero) // Vector3 is non-nullable; comparing to null is always false
+        {
+            transform.position = hit.point;
+        }
+        else
+        {
+            transform.position = ray.GetPoint(3.0f);
+        }
+    }
+}
